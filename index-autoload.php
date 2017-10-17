2798cf2230f32b84f5b9245f748c1260ea45d1d9
Pau <pauiglesias@gmail.com>
2017-10-17 19:42:32 +0200

    Added the new version number.

--- index-autoload.php
+++ index-autoload.php
@@ -3,7 +3,7 @@
 Plugin Name: Index Autoload
 Plugin URI: https://www.littlebizzy.com/plugins/index-autoload
 Description: Adds an index to the autoload in wp_options table via WP-Cron on a daily basis, resulting in a more efficient database and faster site performance.
-Version: 1.0.2
+Version: 1.0.3
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
